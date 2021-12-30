4c3189932c5db41062a8f9f155876ec2adbeccd1
rulerhao <z19970615@gmail.com>
2021-12-30 12:10:40 +0800

    1. Add data files to git. 2. Add comments. 3. Delete no use print.
    From Jake.

--- main.py
+++ main.py
@@ -1,27 +1,15 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import plotly.express as px
 import pandas as pd
-from random import randrange
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-fileName = 'C:/Users/jake/Desktop/耳機孔測試/m8/test.txt'
+# Read data from txt file.
+fileName = 'data/5300.txt'
 f = open(fileName, 'r')
 str2 = f.read().split()
 f.close
 
-dataArr = [int(numeric_string) for numeric_string in str2]  # string array to int array
-xAxisValue = [element for element in range(len(str2))]
+dataArr = [int(numeric_string) for numeric_string in str2]  # Make string array to int array
+xAxisValue = [element for element in range(len(str2))] # Generate XAxis value according to dataArr.index
 
-print(xAxisValue)
-print(dataArr)
 mDict = {
     "x": xAxisValue,
     "y": dataArr
